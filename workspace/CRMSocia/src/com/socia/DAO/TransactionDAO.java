55e1b0acd12cd360905261c1dbdaad6fa9b16726
lazyros <rosa.mendiola.i@gmail.com>
2017-03-25 11:17:12 -0600

    Servidor - Que se realicen los queries

--- workspace/CRMSocia/src/com/socia/DAO/TransactionDAO.java
+++ workspace/CRMSocia/src/com/socia/DAO/TransactionDAO.java
@@ -22,9 +22,43 @@ public class TransactionDAO {
 		
 		for(int index=0; index < queries.size(); index++){
 			//System.out.println(queries.get(index).toString());
-			statement.execute(queries.get(index).toString().toUpperCase());
+			statement.execute(replaceTables(queries.get(index).toString().toUpperCase()));
 		}
 
+	}
+	
+	private String replaceTables(String query){
+		return query.replaceAll("CRM_ADDRESS", "crm_address")
+					.replaceAll("CRM_APPOINTMENT", "crm_appointment")
+					.replaceAll("CRM_BL_DETAILS", "crm_bl_details")
+					.replaceAll("CRM_BRAND", "crm_brand")
+					.replaceAll("CRM_CALL", "crm_call")
+					.replaceAll("CRM_CLIENT", "crm_client")
+					.replaceAll("CRM_CLIENT_ADDRESS", "crm_client_address")
+					.replaceAll("CRM_CLIENT_CONTACT", "crm_client_contact")
+					.replaceAll("CRM_COMPANY_DIVISION", "crm_company_division")
+					.replaceAll("CRM_CONSECUTIVES", "crm_consecutives")
+					.replaceAll("CRM_CONTACT", "crm_contact")
+					.replaceAll("CRM_DAYS_OFF", "crm_days_off")
+					.replaceAll("CRM_DIAGNOSIS", "crm_diagnosis")
+					.replaceAll("CRM_DIRECTORY", "crm_directory")
+					.replaceAll("CRM_EQUIPMENT_TYPE", "crm_equipment_type")
+					.replaceAll("CRM_GOAL", "crm_goal")
+					.replaceAll("CRM_ITEM", "crm_item")
+					.replaceAll("CRM_MODEL", "crm_model")
+					.replaceAll("CRM_OPPORTUNITIES", "crm_opportunities")
+					.replaceAll("CRM_POSITION", "crm_position")
+					.replaceAll("CRM_QUOTATION", "crm_quotation")
+					.replaceAll("CRM_QUOTATION_DETAILS", "crm_quotation_detail")
+					.replaceAll("CRM_REGISTRATION_REQUIREMENT", "crm_registration_requirement")
+					.replaceAll("CRM_REG_REQ", "crm_reg_req")
+					.replaceAll("CRM_REQUIREMENT", "crm_requirement")
+					.replaceAll("CRM_RUN_RATE", "crm_run_rate")
+					.replaceAll("CRM_RUN_RATE_TYPE", "crm_run_rate_type")
+					.replaceAll("CRM_TENDER", "crm_tender")
+					.replaceAll("CRM_USER", "crm_user")
+					.replaceAll("CRM_USER_ROLE", "crm_user_role")
+					.replaceAll("CRM_USER_USERROLE", "crm_user_userrole");
 	}
 	
 	public void commit() throws Exception{
